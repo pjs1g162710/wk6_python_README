466b14e320a2a668b5f97d8129061d7ce50081ab
pjs1g162710 <pipsimpson2022@gmail.com>
2025-03-09 14:06:53 +0000

    edits to check terminal operation on VS code

--- terminal_input.py
+++ terminal_input.py
@@ -6,29 +6,26 @@ from InquirerPy import prompt
 
 
 questions = [
-        {"type": "input", "message": "Project Title:", "name": "title"},
-        {"type": "input", "message": "Project Description:", "name": "desc"},
-        {"type": "input", "message": "Installation Instructions:", "name": "install"},
-        {"type": "input", "message": "Usage Instructions:", "name": "usage"},
+{"type": "input", "message": "Project Title:", "name": "title"},
+{"type": "input", "message": "Project Description:", "name": "desc"},
+{"type": "input", "message": "Installation Instructions:", "name": "install"},
+{"type": "input", "message": "Usage Instructions:", "name": "usage"},
+{"type": "input", "message": "Author details:", "name": "author"},
 
 
-        {
-        "type": "list",
-        "message": "Licence:",
-        "choices": ["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)", "JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)","Creative Commons Licenses (CC0, CC BY, etc.","Unlicense"]
-        },
-        {"type": "confirm", "message": "Confirm Licence is correct? (y/n)"},
+{"type": "list",
+"message": "Licence:",
+"choices": ["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)", "JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)","Creative Commons Licenses (CC0, CC BY, etc.","Unlicense"]},
+{"type": "confirm", "message": "Confirm Licence is correct? (y/n)"}]
 
-       #if "n" in result[2]:
-       #{"type": "list",
-       #"message": "Licence:",
-       # "choices": ["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)", "JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)","Creative Commons Licenses (CC0, CC BY, etc.","Unlicense"]
-       # },
-       # {"type": "confirm", "message": "Confirm Licence is correct? (y/n)"},
-       # else
-       # { "message": "Details are complete"},
+if "n" in questions[5]:
 
-        ]
+    confirm = [{"type": "list",
+    "message": "Licence:"},
+    "choices":["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)","JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)", "Creative Commons Licenses (CC0, CC BY, etc.", "Unlicense"]}
+    {"type": "confirm", "message": "Confirm Licence is correct? (y/n)"}]
+else:
+    [{"message": "Details are complete"}]
 
 #create variables to hold input from
 result = prompt(questions)
@@ -36,6 +33,7 @@ project_title = result["title"]
 project_desc = result["desc"]
 install_ins = result["install"]
 usage_ins = result["usage"]
+authdet = result["author"]
 
 lic_type = result[4]
-confirm = result[5]
+confirmed = result[5]
