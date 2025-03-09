1d18d39a66238f928fb785c515fb7fd0ebf3a1b8
pjs1g162710 <pipsimpson2022@gmail.com>
2025-03-09 19:56:32 +0000

    terminal input file fixed with classic syntax

--- terminal_input.py
+++ terminal_input.py
@@ -1,8 +1,9 @@
 #install Inquirer
 #remember to pip install InquirerPy
 from InquirerPy import prompt
-#from InquirerPy.utils import color_print
-#from InquirerPy import inquirer
+from InquirerPy.utils import color_print
+from InquirerPy import inquirer
+
 
 
 questions = [
@@ -11,29 +12,27 @@ questions = [
 {"type": "input", "message": "Installation Instructions:", "name": "install"},
 {"type": "input", "message": "Usage Instructions:", "name": "usage"},
 {"type": "input", "message": "Author details:", "name": "author"},
+{"type": "list", "message": "Licence:","name":"licence",
+"choices": lambda result: ["MIT License",
+             "Apache License 2.0",
+               "GNU General Public License (GPL v3)",
+                 "JGNU Lesser General Public License (LGPL v3)",
+                 "Mozilla Public License 2.0 (MPL 2.0)",
+                 "Creative Commons Licenses (CC0, CC BY, etc.",
+                 "Unlicense"]},
+{"type": "confirm", "message": "Confirm Licence is correct y/n?", "default": True, "name":"confirm"}]
+result = prompt(questions)
 
-
-{"type": "list",
-"message": "Licence:",
-"choices": ["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)", "JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)","Creative Commons Licenses (CC0, CC BY, etc.","Unlicense"]},
-{"type": "confirm", "message": "Confirm Licence is correct? (y/n)"}]
-
-if "n" in questions[5]:
-
-    confirm = [{"type": "list",
-"message": "Licence:",
-"choices": ["MIT License", "Apache License 2.0", "GNU General Public License (GPL v3)", "JGNU Lesser General Public License (LGPL v3),""Mozilla Public License 2.0 (MPL 2.0)","Creative Commons Licenses (CC0, CC BY, etc.","Unlicense"]},
-{"type": "confirm", "message": "Confirm Licence is correct? (y/n)"}]
-else:
-    [{"type":"input","message": "Details are complete"}]
+#works to here
 
 #create variables to hold input from
-result = prompt(questions)
-project_title = result["title"]
+
+project_title =result["title"]
 project_desc = result["desc"]
 install_ins = result["install"]
 usage_ins = result["usage"]
 authdet = result["author"]
 
-lic_type = result[4]
-confirmed = result[5]
+lic_type = result["licence"]
+confirmed = result["confirm"]
+
